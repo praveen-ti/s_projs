81153489b395ee2d021c47794c07c20eef3fead3
Praveen <praveen.ts@titechnologies.org>
2016-01-21 12:44:27 +0530

    email template

--- api/controllers/TestController.js
+++ api/controllers/TestController.js
@@ -7,8 +7,30 @@
 
 module.exports = {
     index: function (req, res) {
+        var email_to = "useremailtestacc@gmail.com";
+        var email_subject = 'Zentiera - Email verify';
+        var email_template = 'email_verify';
+        var email_context = {display_name: "result.name", email: "result.email", link: "result"};
+        UserService.emailSend(email_to, email_subject, email_template, email_context, function (err, sendresult) {
+            if (err)
+            {
+                return res.json(200, {status: 2, message: 'some error occured', error_details: sendresult});
+                sails.log.debug('Some error occured ' + sendresult);
+            }
+            else {
+                console.log("User -> email send");
+                //console.log(result);
+                console.log(email_to);
+                console.log(email_subject);
+                console.log(email_template);
+                console.log(email_context);
+                return res.json(200, {status: 1, message: 'succes', details: 'details'});
+            }
+        });
         return res.view("test", {test: 'testing'});
     }
+
+
 
 };
 

601cf09873047ce5259867c564273adb894bad50
Titto <titto.xavier@titechnologies.org>
2016-02-02 11:32:47 +0530

    conflict changes

--- assets/js/adminControllers.js
+++ assets/js/adminControllers.js
@@ -5,26 +5,42 @@ var adminControllers = angular.module('adminControllers', []);
 
 adminControllers.controller('adminLoginCtrl', function ($scope, $routeParams, $rootScope, $http, $location) {
 
-    $scope.login = function (users) {
 
-        var credentials = {
+     $scope.loginSubmit = function() {
+        var params = {
             username: $scope.username,
             password: $scope.password
         };
+        if (username && password) {
+            $http.post($rootScope.STATIC_URL + 'admins/adminLogin',params).success(function(data, status, headers, config) {
 
-        $http.post($rootScope.STATIC_URL + 'admins/adminLogin', credentials).success(function (response) {
-            if (response.status)
-            {
-                $location.path('/index');
-            } else {
-                $scope.invalidlogin = true;
-            }
-            console.log(response);
-        }).error(function (err) {
-            console.log("failure");
-        });
+                                console.log("SUCCESS _______________");
+                                /*
+                                                if(data.status == 1)
+                                                {
+                                                    $window.location.href = '/admin/dashboard';
+                                                }
+                                                else
+                                                {
+                                                    $scope.login_error_message = "Invalid login credentials";
+                                                }
+                                 */
+                    }).error(function() {
 
-    }
+                        console.log("EROOR _______________");
+
+                    });
+        }
+        else if(username){
+                $scope.login_error_message = "Please Enter the password";
+              }
+        else if(password){
+                $scope.login_error_message = "Please Enter the Username";
+              }
+        else {
+               $scope.login_error_message = "All fields are mandatory";
+        }
+    };
 
 });
 

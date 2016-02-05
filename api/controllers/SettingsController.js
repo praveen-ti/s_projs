3405f4cb101e9021bea34393a3c7e14a7f4b13b1
Titto <titto.xavier@titechnologies.org>
2016-02-05 21:00:38 +0530

    fixed the conflicts

--- api/controllers/SettingsController.js
+++ api/controllers/SettingsController.js
@@ -7,11 +7,49 @@
 
 module.exports = {
 
+
+/*===================================================================================================================================
+     Get all Settings
+  ====================================================================================================================================*/
+
+    getSettingsList: function (req, res) {
+
+        AdmintokenService.checkToken(req.body.token, function (err, tokenCheck) {
+
+            if (err)
+            {
+                return res.json(200, {status: 2, message: 'some error occured', error_details: tokenCheck});
+            }
+            else
+            {
+                if (tokenCheck.status == 1)
+                {
+                    var query = "SELECT * FROM settings ORDER BY createdAt DESC";
+                    Settings.query(query, function (err, result) {
+                        if (err)
+                        {
+                            return res.json(200, {status: 2, error_details: err});
+                        }
+                        else
+                        {
+                            console.log(result);
+                            return res.json(200, {status: 1, message: "success", data: result});
+                        }
+                    });
+                }
+                else
+                {
+                    return res.json(200, {status: 3, message: 'token expired'});
+                }
+            }
+        });
+    },
+
 /*===================================================================================================================================
                                                    Create/Add a Settings
  ====================================================================================================================================*/
 
-
+/*
     addSettings : function(req, res) {
 
            AdmintokenService.checkToken(req.body.token, function(err, tokenCheck) {
@@ -45,13 +83,56 @@ module.exports = {
                     }
             });
     },
+*/
+
+
+ /*===================================================================================================================================
+     Get current Settings
+     ====================================================================================================================================*/
+
+
+    getSettingsDetails: function (req, res) {
+var request = req.body.request;
+//console.log(request.adminId);
+//console.log(req.body.token);
+        AdmintokenService.checkToken(req.body.token, function (err, tokenCheck) {
+
+            if (err)
+            {
+                return res.json(200, {status: 2, message: 'some error occured', error_details: tokenCheck});
+            }
+            else
+            {
+                if (tokenCheck.status == 1)
+                {
+                    Settings.findOne({id: request.settingsId}).exec(function findCB(err, result) {
+                        if (err)
+                        {
+                            return res.json(200, {status: 2, error_details: err});
+                        }
+                        else
+                        {
+
+                            return res.json(200, {status: 1, data: result});
+                        }
+
+                    });
+                }
+                else
+                {
+                    return res.json(200, {status: 3, message: 'token expired'});
+                }
+            }
+        });
+    },
+
 
 /*===================================================================================================================================
                                                       Edit Settings
  ====================================================================================================================================*/
 
 
-    updateSettings : function(req, res) {
+    updateSettingsDetails : function(req, res) {
 
           AdmintokenService.checkToken(req.body.token, function(err, tokenCheck) {
 
